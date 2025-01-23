cc10198fda163a2a0703df681e9e870b7c47cf25
rahul01992 <97080308+rahul01992@users.noreply.github.com>
2025-01-23 22:38:20 +0530

    Update database.configs.js

--- backend/src/configs/database.configs.js
+++ backend/src/configs/database.configs.js
@@ -3,7 +3,7 @@ import { APP_NAME } from "../constants.js";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(`${process.env.DATABASE_URI}`);
+    await mongoose.connect(`mongodb+srv://rahul01992:13KGvI9y046mbSgj@cluster0.dznyo.mongodb.net/API`);
     console.log(`DATABASE CONNECTED`);
   } catch (error) {
     console.log(`!!! DATABASE CONNECTION ERROR !!!`);
